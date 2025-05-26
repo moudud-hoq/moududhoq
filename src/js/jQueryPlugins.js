f2fb0ee57329c075a6a67f5295a660c16fe9ba7c
moududulhaque <moududulhaque@gmail.com>
2025-05-26 13:30:24 +0600

    Applly jQuery Plugin in Hero Section's Title

---
+++ src/js/jQueryPlugins.js
@@ -0,0 +1,13 @@
+  document.addEventListener("DOMContentLoaded", function () {
+    new Typed('#typed-text', {
+      strings: [
+        "A passionate developer",
+        "Creating awesome web experiences",
+        "Loving to build with code"
+      ],
+      typeSpeed: 50,
+      backSpeed: 30,
+      backDelay: 1000,
+      loop: true
+    });
+  });
