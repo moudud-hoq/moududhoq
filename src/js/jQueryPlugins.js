29bc9e61bfdddc801d1d411aacc4e800d2a669c9
moududulhaque <moududulhaque@gmail.com>
2025-05-26 13:32:53 +0600

    Improve the typed plugin

--- src/js/jQueryPlugins.js
+++ src/js/jQueryPlugins.js
@@ -1,13 +1,25 @@
-  document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function () {
     new Typed('#typed-text', {
-      strings: [
-        "A passionate developer",
-        "Creating awesome web experiences",
-        "Loving to build with code"
-      ],
-      typeSpeed: 50,
-      backSpeed: 30,
-      backDelay: 1000,
-      loop: true
+        strings: [
+            "A passionate developer",
+            "Loving to build with code and cms",
+            "Creating awesome web experiences",
+        ],
+        typeSpeed: 50,
+        backSpeed: 30,
+        backDelay: 1000,
+        loop: true
     });
-  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    new Typed('#typed-text2', {
+        strings: [
+            "Moudud Hoq",
+        ],
+        typeSpeed: 80,
+        backSpeed: 80,
+        backDelay: 1000,
+        loop: true
+    });
+});
