f406b074dc2afaf78bb15ab73751a5cda7ce386d
moududulhaque <moududulhaque@gmail.com>
2025-06-20 07:22:35 +0600

    Entire Website Update and Clean

--- src/js/jQueryPlugins.js
+++ src/js/jQueryPlugins.js
@@ -1,18 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
-    new Typed('#typed-text', {
-        strings: [
-            "A passionate developer",
-            "Loving to build with code and cms",
-            "Creating awesome web experiences",
-        ],
-        typeSpeed: 50,
-        backSpeed: 30,
-        backDelay: 1000,
-        loop: true
-    });
-});
-
-document.addEventListener("DOMContentLoaded", function () {
     new Typed('#typed-text2', {
         strings: [
             "Moudud Hoq",
